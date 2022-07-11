3d25ef1abf18952bd97cde5111e98dbbf2127ec7
Anthony <aajohnso.coms@gmail.com>
2022-07-10 21:15:11 -0600

    added function to append form submitted books

--- top-library.js
+++ top-library.js
@@ -10,29 +10,34 @@ function Book(title, author, numPages) {
 
 function addBook(book) {
     myLibrary.push(book);
+    displayBook(book);
+}
+
+function displayBook(book) {
+    let bookCard = document.createElement("div");
+    let bookCardTitle = document.createElement("h2");
+    let bookCardAuthor = document.createElement("p");
+    let bookCardPages = document.createElement("p");
+
+    bookCard.classList.add("book-card");
+    bookCardAuthor.classList.add("author");
+    bookCardPages.classList.add("pages");
+
+    bookCardTitle.innerText = book.title;
+    bookCardAuthor.innerText = book.author;
+    bookCardPages.innerText = book.numPages;
+
+    bookCard.appendChild(bookCardTitle);
+    bookCard.appendChild(bookCardAuthor);
+    bookCard.appendChild(bookCardPages);
+
+    bookContainer.appendChild(bookCard);
 }
 
 function showBooks() {
     myLibrary.forEach(
         book => {
-            let bookCard = document.createElement("div");
-            let bookCardTitle = document.createElement("h2");
-            let bookCardAuthor = document.createElement("p");
-            let bookCardPages = document.createElement("p");
-
-            bookCard.classList.add("book-card");
-            bookCardAuthor.classList.add("author");
-            bookCardPages.classList.add("pages");
-
-            bookCardTitle.innerText = book.title;
-            bookCardAuthor.innerText = book.author;
-            bookCardPages.innerText = book.numPages;
-
-            bookCard.appendChild(bookCardTitle);
-            bookCard.appendChild(bookCardAuthor);
-            bookCard.appendChild(bookCardPages);
-
-            bookContainer.appendChild(bookCard);
+            displayBook(book);
         }
     );
 }
