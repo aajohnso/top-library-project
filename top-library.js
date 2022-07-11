907d57e948bfa6664f0427e1380e02e7cdc17b78
Anthony <aajohnso.coms@gmail.com>
2022-07-10 23:24:19 -0600

    added remove button functionality

--- top-library.js
+++ top-library.js
@@ -1,6 +1,9 @@
 let myLibrary = [];
 const bookContainer = document.getElementById("books-container");
 const bookForm = document.getElementById("book-form");
+const bookFormBtn = document.getElementById("book-form-btn");
+
+bookForm.style.display = "none";
 
 function Book(title, author, numPages) {
     this.title = title;
@@ -13,30 +16,43 @@ function addBook(book) {
 }
 
 function displayBook(book, index) {
+    
+    let bookIndex = index;
+    
     let bookCard = document.createElement("div");
     let bookCardTitle = document.createElement("h2");
     let bookCardAuthor = document.createElement("p");
     let bookCardPages = document.createElement("p");
+    let bookCardRemove = document.createElement("button");
 
     bookCard.classList.add("book-card");
     bookCardAuthor.classList.add("author");
     bookCardPages.classList.add("pages");
+    bookCardRemove.classList.add("remove-book");
 
     bookCardTitle.innerText = book.title;
     bookCardAuthor.innerText = book.author;
     bookCardPages.innerText = book.numPages;
+    bookCardRemove.innerText = "Remove";
 
     bookCard.appendChild(bookCardTitle);
     bookCard.appendChild(bookCardAuthor);
     bookCard.appendChild(bookCardPages);
+    bookCard.appendChild(bookCardRemove);
 
-    bookCard.setAttribute("book-index", index);
+    bookCard.setAttribute("book-index", bookIndex);
+
+    bookCardRemove.addEventListener("click", () => {
+        myLibrary.splice(bookIndex, 1);
+        showBooks();
+    });
 
     bookContainer.appendChild(bookCard);
 }
 
 function showBooks() {
     let bookIndex = 0;
+    bookContainer.innerHTML = "";
     myLibrary.forEach(
         book => {
             displayBook(book, bookIndex);
@@ -53,8 +69,6 @@ function submitBook(event) {
 
     let newBook = new Book(newBookTitle, newBookAuthor, newBookPages);
 
-    bookContainer.innerHTML = "";
-    
     addBook(newBook);
     showBooks();
 
@@ -63,4 +77,11 @@ function submitBook(event) {
 
 }
 
+function showForm() {
+    if (bookForm.style.display == "none") {
+        bookForm.style.display = "block";
+    }
+}
+
 bookForm.addEventListener("submit", submitBook);
+bookFormBtn.addEventListener("click", showForm);
